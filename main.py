8a2ebdf4e3701883b4813c7420897528401bf33f
JAFigueroaAcero <98606608+JAFigueroaAcero@users.noreply.github.com>
2022-12-10 09:42:59 -0600

    update in k

--- main.py
+++ main.py
@@ -11,29 +11,32 @@ def S(x,t,i):
     b = i*(99461*e(3470,4399,x) + 56729 * e(21730,3500,x) + 1190 * e(33990,600,x) + 400 * e(53690,200,x))
     return (1/(24))*(1-np.cos(np.pi/(12) * t))*b
 
+def k(t,kt):
+    return kt*(1/(24))*(1-np.cos(np.pi/(12) * t))
 
 def main():
-    Ttotal = 80
+    Ttotal = 140
     dt = 0.05
     dx = 1000
     L = 200000
     mu = 0.26
     u = 0.5*3600
-    q = 0.01
-    i = 0.131
+    q = 0
+    i = 70
+    kt = 0.5
     xlen = int(L/dx)
     tlen = int(Ttotal/dt)
     C = [[0 for a in range(xlen)]]
     for t in range(1,tlen):
         loc_l = []
-        loc_1 = dt * (2*mu * (C[t-1][1]-C[t-1][0]- 2*dx*q)/(dx**2) - u*q + S(0,t*dt,i)) + C[t-1][0]
+        loc_1 = dt * (2*mu * (C[t-1][1]-C[t-1][0]- 2*dx*q)/(dx**2) - u*q + S(0,t*dt,i)) + C[t-1][0] - k(dt*(t-1),kt) * C[t-1][0]
         if loc_1 < 0:
             loc_l.append(0)
         else:
             loc_l.append(loc_1)
         
         for x in range(1,xlen-1):
-            loc_2 = dt * (mu * (C[t-1][x+1]-2*C[t-1][x] + C[t-1][x-1])/(dx**2) - u*(C[t-1][x+1]-C[t-1][x-1])/(2*dx) + S(x*dx,t*dt,i)) + C[t-1][x]
+            loc_2 = dt * (mu * (C[t-1][x+1]-2*C[t-1][x] + C[t-1][x-1])/(dx**2) - u*(C[t-1][x+1]-C[t-1][x-1])/(2*dx) + S(x*dx,t*dt,i)) + C[t-1][x] - k(dt*(t-1),kt) * C[t-1][x]
             if loc_2 < 0:
                 loc_l.append(0)
             else:
@@ -52,7 +55,7 @@ def main():
     ax.zaxis.set_major_formatter('{x:.02f}')
     ax.set_xlabel(r'$Distancia-(km)$')
     ax.set_ylabel(r'$Tiempo-(h)$')
-    ax.set_zlabel(r'$Contaminante - \left(\frac{kg}{L}\right)$')
+    ax.set_zlabel(r'$Contaminante - \left(\frac{g}{L}\right)$')
     fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
     
